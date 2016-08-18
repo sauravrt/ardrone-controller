4b81538454c8afb7c6d0b656a7cfebd690387087
Saurav Tuladhar <saurav@arcon.com>
2016-08-18 13:48:49 -0400

    Bug fix bind port, send msg format

--- ardrone_con.py
+++ ardrone_con.py
@@ -17,14 +17,14 @@ class ArDrone:
 
         # Create socket to talk to ardrone on control port 5556
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.s.bind(self._address)
+        self.s.bind(('', 5554))
 
     def _send_command(self, cmd):
-        print('INFO: Sending: {}'.format(cmd.strip()))
-        self.s.sendto(cmd, self._address)
+        print("INFO: Sending: '%s'" % cmd.strip())
+        self.s.sendto(cmd.encode('utf-8'), self._address)
         self._seqno += 1
 
-    def _set_bits(lst):
+    def _set_bits(self, lst):
         res = 0
         for b in lst + [18, 20, 22, 24, 28]:
             res |= (1 << b)
@@ -37,12 +37,12 @@ class ArDrone:
     def takeoff(self):
         self._send_command("AT*FTRIM=%d\r" % self._seqno)
         takeoff_cmd = self._set_bits([9])
-        for i in xrange(1, 25):
+        for i in range(1, 25):
             self._send_command("AT*REF=%d,%d\r" % (self._seqno, takeoff_cmd))
 
     def land(self):
         land_cmd = self._set_bits([])
-        for i in xrange(1, 25):
+        for i in range(1, 25):
             self._send_command("AT*REF=%d,%d\r" % (self._seqno, land_cmd))
 
     def emergency(self):
